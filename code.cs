4fab637f3b8969f7117532c0cbae78fc603f9392
murillo dev72 <148728763+murillodev72@users.noreply.github.com>
2023-11-30 13:35:27 -0300

    Add files via upload

---
+++ code.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace GeradorDeRetangulo{
+class classe{
+static void Main(String[] args){
+
+Console.WriteLine("bem vindo ao gerador de retângulo");
+
+Console.Write("insira o número de linhas: ");
+int linhas = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("insira o número de colunas: ");
+int colunas = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("insira o símbolo ou caracter: ");
+String simbolo = Console.ReadLine();
+
+
+for(int i = 0; i < linhas; i++){
+
+	for(int j = 0; j < colunas; j++){
+	Console.Write(simbolo);
+	}
+	Console.WriteLine();
+}
+
+}
+}
+}
